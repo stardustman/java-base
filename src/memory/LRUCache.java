50b363e17b3f30645700f5f24ce05e30867b96a4
stardust <aoyunyoung@gmail.com>
2019-05-18 16:16:34 +0800

    add cache replacement policy LRU(least recently used)

---
+++ src/memory/LRUCache.java
@@ -0,0 +1,91 @@
+package memory;
+//https://www.geeksforgeeks.org/lru-cache-implementation/
+/* We can use Java inbuilt Deque as a double 
+ended queue to store the cache keys, with 
+the descending time of reference from front 
+to back and a set container to check presence 
+of a key. But remove a key from the Deque using 
+remove() , it takes O(N) time. This can be 
+optimized by storing a reference (iterator) to 
+each key in a hash map. */
+import java.util.Deque; 
+import java.util.HashSet; 
+import java.util.LinkedList; 
+import java.util.Iterator; 
+public class LRUCache { 
+	// store keys of cache 
+	static Deque<Integer> dq; 
+	// store references of key in cache 
+	static HashSet<Integer> map; 
+	//maximum capacity of cache 
+	static int csize; 
+	
+	LRUCache(int n) 
+	{ 
+		dq=new LinkedList<>(); 
+		map=new HashSet<>(); 
+		csize=n; 
+	} 
+	
+	/* Refers key x with in the LRU cache */
+	public void refer(int x) 
+	{ 
+		//cache miss
+		if(!map.contains(x)) 
+		{ 
+			if(dq.size()==csize) 
+			{ 
+				// 队尾 最后被访问的时间距现在最远的块
+				int last=dq.removeLast(); 
+				map.remove(last); 
+			} 
+		} 
+		//cache hit
+		else
+		{ 
+			/* The found page may not be always the last element, even if it's an 
+			intermediate element that needs to be removed and added to the start 
+			of the Queue */
+			int index =0 , i=0; 
+			Iterator<Integer> itr = dq.iterator(); 
+			while(itr.hasNext()) 
+			{ 
+				if(itr.next()==x) 
+				{ 
+					index = i; 
+					break; 
+				} 
+				i++; 
+			} 
+			//删除
+			dq.remove(index); 
+		} 
+		//加到队首
+		dq.push(x);
+		// 最后结果都是 cache
+		map.add(x); 
+	} 
+	
+	// display contents of cache 
+	public void display() 
+	{ 
+		Iterator<Integer> itr = dq.iterator(); 
+		while(itr.hasNext()) 
+		{ 
+			System.out.print(itr.next()+" "); 
+		} 
+	} 
+	
+	
+	public static void main(String[] args) { 
+		LRUCache ca=new LRUCache(4); 
+		ca.refer(1); 
+		ca.refer(2); 
+		ca.refer(3); 
+		ca.refer(1); 
+		ca.refer(4); 
+		ca.refer(5); 
+		ca.display();	 
+	} 
+} 
+//This code is contributed by Gaurav Tiwari 
