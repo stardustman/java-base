312f5b696bce9c85311fb032d4e6d915733c35a6
stardust <aoyunyoung@gmail.com>
2019-06-03 15:34:38 +0800

    枚举实现单例

--- src/pattern/singleton/enumclass/Singleton.java
+++ src/pattern/singleton/enumclass/Singleton.java
@@ -3,5 +3,10 @@ package pattern.singleton.enumclass;
 public enum Singleton {
     INSTANCE;
     public void whateverMethod() {
+    	System.out.println("what's the fuck?");
     }
+    
+    public static void main(String[] args) {
+		Singleton.INSTANCE.whateverMethod();
+	}
 }
