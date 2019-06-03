39d51cf0988e56559058edec8a2ee823a8ae2e5f
stardust <aoyunyoung@gmail.com>
2019-06-03 17:23:04 +0800

    查看 InvocationHandler invoke 方法,以及参数来源

--- src/pattern/proxy/jdk/dynamic/InvocationSubject.java
+++ src/pattern/proxy/jdk/dynamic/InvocationSubject.java
@@ -15,10 +15,12 @@ public class InvocationSubject implements InvocationHandler {
         target = subject;
     }
 
-    //
+    // 这个实际上方法供 $Proxy0 实现 Subject 接口里面的方法调用.
     public Object invoke(Object proxy, Method method, Object[] args)
             throws Throwable {
-        System.out.println("log : " + method.getName() + " invoked with " + args);
+    	// args 是 Subject 接口 定义的方法的参数列表,由  $Proxy0  的实现的方法传递.
+    	// 实际上也就是调用生成的代理对象的方法时的参数.
+        System.out.println("log : " + method.getName() + " invoked with arguments object is:" + args);
         System.out.println("相当于后门,改变生成的代理对象的行为!");
         
         //调用原来对象的方法
